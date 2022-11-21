c11a01c4a59b76c46f9035f3ad2426cbb24c775a
Johlar0185 <johlar1988@gmail.com>
2022-11-21 20:10:03 +0100

    fetched data

---
+++ js/ambassadors.js
@@ -0,0 +1,40 @@
+async function getData() {
+  let result = await fetch(
+    "https://johannela.dk/cartel/wp-json/wp/v2/ambassador?_embed"
+  );
+  dataReceived(await result.json());
+}
+
+function dataReceived(data) {
+  //do something with the data
+  console.log(data);
+  data.forEach(showSingleAmbassador);
+  //console.log(hello, post);
+}
+
+function showSingleAmbassador(ambassador) {
+  //template
+  const template = document.querySelector("template").content;
+  //clone
+  const clone = template.cloneNode(true);
+  console.log(ambassador);
+  //put the content into it
+  //ambassador name
+  clone.querySelector(".name").textContent = ambassador.username.rendered;
+  //ig username
+  clone.querySelector(".ig__username span").textContent =
+    ambassador.instagram_user;
+  //ig link
+  clone.querySelector(".ig__link").textContent = ambassador.instagram_link;
+  //image
+  clone.querySelector("img").src =
+    ambassador._embedded[
+      "wp:featuredmedia"
+    ][0].media_details.sizes.medium.source_url;
+  //brand
+
+  const parent = document.querySelector("main");
+  parent.appendChild(clone);
+}
+
+getData();
